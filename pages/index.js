bf555360b5aff15d1f68b26251ebf1814a29dd1c
Joao Sabino <joao.sabino.505@gmail.com>
2025-12-04 20:40:44 -0300

    arrumando frase da pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <h1>Mãe, seu amor é meu porto seguro. Obrigado por tudo que você é.
+        Se gostou da mensagem da uma risadinha!
+    </h1>;
+    
+}
+
+export default Home;
